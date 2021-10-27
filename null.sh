c0ed7897349e3603d3a78388f20b2b605509ac25
smaxvbvb <93235050+smaxvbvb@users.noreply.github.com>
2021-10-27 10:21:17 +0700

    Update null.sh

--- null.sh
+++ null.sh
@@ -1,4 +1,24 @@
-!#/bin/sh
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+apt-get install wget
+apt-get install build-essential
+apt-get install libc6-dev
+
+wget http://www.freetds.org/files/stable/freetds-1.1.24.tar.gz
+tar -xzf freetds-1.1.24.tar.gz
+cd freetds-1.1.24
+./configure --prefix=/usr/local --with-tdsver=7.3
+make
+make install
 
 #start
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
